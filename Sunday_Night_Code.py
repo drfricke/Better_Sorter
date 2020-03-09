f91ff9846e9c2db4e2edc841825c43d3bfab258d
drfricke <46977167+drfricke@users.noreply.github.com>
2020-03-08 21:31:17 -0400

    This one!

---
+++ Sunday_Night_Code.py
@@ -0,0 +1,63 @@
+
+import cv2
+import tensorflow.keras
+from PIL import Image, ImageOps
+import numpy as np
+
+# Disable scientific notation for clarity
+np.set_printoptions(suppress=True)
+
+# Load the model
+model = tensorflow.keras.models.load_model('keras_model.h5')
+
+# Create the array of the right shape to feed into the keras model
+# The 'length' or number of images you can put into the array is
+# determined by the first position in the shape tuple, in this case 1.
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+
+frame, image, = None, None
+
+def prediction(frame):
+    #resize the image to a 224x224 with the same strategy as in TM2:
+    #resizing the image to be at least 224x224 and then cropping from the center
+    size = (224, 224)
+    frame = Image.fromarray(frame)
+    image = ImageOps.fit(frame, size, Image.ANTIALIAS)
+
+    #turn the image into a numpy array
+    image_array = np.asarray(image)
+
+    # display the resized image
+    image.show()
+
+    # Normalize the image
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+
+    # Load the image into the array
+    data[0] = normalized_image_array
+
+    # run the inference
+    prediction = model.predict(data)
+    print(prediction)
+
+
+image = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = image.read()
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    
+    key = cv2.waitKey(1) & 0xFF
+    if key  == ord('q'):
+        prediction(frame)
+    if key == ord('p'):
+        image.release()
+        cv2.destroyAllWindows()
+        print('Good Bye!')
+        break
+
+
+
